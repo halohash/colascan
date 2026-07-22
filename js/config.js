f484a806c57cd4c6d9d886c7dde32fb2c1be0d88
HaloHash <bee.the.guh22@gmail.com>
2026-07-21 20:35:53 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -29,7 +29,7 @@ var slideSettings = {
 
 var audioSettings = {
   enableMusic: true,
-  shuffle: false,
+  shuffle: true,
   randomStart: true,
-  order: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],//1 to 25
+  order: [1,2,3,4],
 }
